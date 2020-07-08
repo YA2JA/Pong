ae4fd1f6b92674698bc2abccde2b6032fa0c18a1
Jak Oganesyan <jakoganesyan@gmail.com>
2020-07-08 17:48:56 +0200

    AI shud work better

--- Scripts/AI.cs
+++ Scripts/AI.cs
@@ -1,32 +1,26 @@
-﻿using System;
+﻿using System.Collections;
 using UnityEngine;
+
 public class AI : MonoBehaviour
 {
-    [SerializeField] private float _speed=0f;
-    [SerializeField] private GameObject _followedObject = null;
-    [SerializeField] [Range(0,2)] private float _missTakeAmplitude, _missTakeMin = 0f;
+    [SerializeField] private float _speed = 0f;
+    [SerializeField] private GameObject _ball = null;
+    [SerializeField] [Range(0,2)] private float _missTakeAmplitude = 0f;
     private float _missTake;
-    private float _targetPosition, _direction, _diffrence;
     private float _move => _speed*_direction*_diffrence*Time.deltaTime;
-    private Func<float, float, float> compute = (a, b) => Mathf.Sign(a)*(Mathf.Abs(a)+Mathf.Abs(b));
-    private void Update()
-    {
-        _targetPosition = getTargetPos();
-        _direction = Mathf.Sign(_targetPosition-transform.position.y);
-        _diffrence = Mathf.Clamp(Mathf.Abs(_targetPosition-this.transform.position.y), 0f, 1f);
-        transform.position = new Vector2 (transform.position.x, transform.position.y+_move);   
-    }
+    private float _y => transform.position.y;
+    float _targetPosition => _ball.transform.position.y+_missTake;
+    float _direction => Mathf.Sign(_targetPosition-_y);
+    float _diffrence => Mathf.Clamp(Mathf.Abs(_targetPosition-_y), 0f, 1f);
+    bool IsBallComming => (_ball.GetComponent<Ball>()._direction < 0) ? (transform.position.x-_ball.transform.position.x < 0) : (transform.position.x-_ball.transform.position.x > 0);
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.GetComponent<Ball>() != null){
-
-            _missTake = Mathf.Clamp(_missTakeMin, 0f, _missTakeMin);
-
-        } //*compute(UnityEngine.Random.Range(-_missTakeAmplitude, _missTakeAmplitude),_missTakeMin);
+        if(other.GetComponent<Ball>() != null) _missTake = Random.Range(0, _missTakeAmplitude);
     }
-    private float getTargetPos(){
 
-        return _followedObject.transform.position.y+_missTake*Mathf.Sign(-transform.position.y);
-
+    private void FixedUpdate() {
+        if (IsBallComming){
+            transform.position = new Vector2 (transform.position.x, _y+_move);   
+        }
     }
 }
--- Scripts/Ball.cs
+++ Scripts/Ball.cs
@@ -1,19 +1,16 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
     [SerializeField] private float _speed = 0f;
-    [SerializeField] private float _increaseIntensity = 0f;
+    [SerializeField] private float _increaseTimer = 0f;
     private Rigidbody2D _rigidbody;
-    private float _timer;
-    private float _direction = 1f;
-    private void Awake() 
+    public float _direction = 1f;
+    private void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
-        _timer = 1/_increaseIntensity;
-        _rigidbody.velocity = new Vector2 (_speed*_direction, _rigidbody.velocity.y);
+        _rigidbody.velocity = new Vector2 (_speed*_direction, 0);
         StartCoroutine(speedIncrease());    
     }
     private void OnTriggerEnter2D(Collider2D other){
@@ -27,16 +24,16 @@ public class Ball : MonoBehaviour
         else
         {
             distanceFromCenter =  other.transform.position.x-this.transform.position.x;
-            _rigidbody.velocity = new Vector2 (_speed*_direction, -_rigidbody.velocity.y);
+            _rigidbody.velocity = new Vector2 (_rigidbody.velocity.x, -_rigidbody.velocity.y);
         }
     }
+
     private IEnumerator speedIncrease()
     {
-        while (true)
+        while (_increaseTimer != 0)
         {
             _speed =  _speed+1;
-            _timer = 1/_increaseIntensity; 
-            yield return new WaitForSeconds(_timer);
+            yield return new WaitForSeconds(_increaseTimer);
         }
     } 
 }
